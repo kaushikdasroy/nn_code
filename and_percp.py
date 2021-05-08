e96a24203f56d0c4f3d07dc5c3f971acfa189693
Ubuntu <ubuntu@ip-172-31-12-100.us-west-2.compute.internal>
2021-05-08 03:39:08 +0000

    first commit

---
+++ and_percp.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+weight1=0.5
+weight2=0.5
+bias=-1
+
+inputs=[(0,0),(0,1),(1,0),(1,1)]
+outputs=[False,False,False,True]
+
+together=[]
+
+for input, output in zip(inputs, outputs):
+    print(input,output)
+    result = weight1*input[0] + weight2*input[1]+bias
+    x = int(result >= 0)
+    """print(x)"""
+    is_correct_string= 'Yes' if x == output else 'No'
+    print(is_correct_string)
+    together.append([input[0], input[1], result, x, is_correct_string])
+output_frame= pd.DataFrame(together,columns=['Input 1','Input 2','Linear Combination','output','is_correct_string'])
+print(output_frame)
+
+
